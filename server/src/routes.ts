381078b167a26ade694f33860e82deacfe0df52e
negreyscow <leeo.apc@gmail.com>
2020-06-02 21:55:15 -0300

    create tables

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,45 @@
-import express from 'express'
+import express, { response } from 'express'
+import knex from './database/connection'
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'htlo'})
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*')
+
+    const serializedItems = items.map(item => ({
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    ))
+
+    return res.json(serializedItems)
 })
 
+routes.post('/points', async (req, res) => {
+    const { 
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+     } = req.body
+
+     await knex('points').insert({
+        name,
+        email,
+        image: 'fake',
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+     })
+
+     return res.json({success: true})
+})
+  
 export default routes
